4d83718082c856cb0cfe902a357035db5112ad24
kmoxville <kmoxville@gmail.org>
2019-05-24 22:09:00 +0600

    Some helper functions

--- src/main/java/com/kmoxvilleEntertainmentGroup/Main.java
+++ src/main/java/com/kmoxvilleEntertainmentGroup/Main.java
@@ -2,62 +2,45 @@ package com.kmoxvilleEntertainmentGroup;
 
 //import org.apache.commons.cli.*;
 
-import com.kmoxvilleEntertainmentGroup.DataProviders.DataProvider;
-import com.kmoxvilleEntertainmentGroup.DataProviders.DataProviderFactory;
-import com.kmoxvilleEntertainmentGroup.DataProviders.DataProviderNotFoundException;
-import com.kmoxvilleEntertainmentGroup.DataProviders.DataProviderType;
-import org.apache.commons.cli.*;
+import com.kmoxvilleEntertainmentGroup.DataProviders.*;
+import com.kmoxvilleEntertainmentGroup.Utils.Options;
+import static com.kmoxvilleEntertainmentGroup.Utils.HelperUtils.*;
+import org.apache.commons.cli.ParseException;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Options options = new Options();
-
-        Option propertiesSourceOption = Option.builder("p")
-                .required(true)
-                .longOpt("properties-source")
-                .desc("Source of properties(json, xml, cvs file)")
-                .hasArg(true)
-                .type(String.class)
-                .build();
-
-        Option animalsSourceOption = Option.builder("a")
-                .required(true)
-                .longOpt("animals-source")
-                .desc("Source of animals(json, xml, cvs file)")
-                .hasArg(true)
-                .type(String.class)
-                .build();
-
-        options.addOption(propertiesSourceOption)
-                .addOption(animalsSourceOption);
-
-        CommandLineParser parser = new DefaultParser();
-        HelpFormatter formatter = new HelpFormatter();
-        CommandLine cmd = null;
-
         try {
-            cmd = parser.parse(options, args);
-        } catch (ParseException e) {
-            System.out.println(e.getMessage());
-            formatter.printHelp("accontanimals", options);
-            System.exit(1);
+            Options.processCommandLine(args);
+        }
+        catch (ParseException e) {
+            print(e.getMessage());
+            exit(-1);
         }
 
-        String propFilePath = cmd.getOptionValue("properties-source");
-        String animalsFilePath = cmd.getOptionValue("animals-source");
+        DataProvider propDataProvider = null;
+        DataProvider animalsDataProvider = null;
 
         try {
-            DataProvider propDataProvider = DataProviderFactory
-                    .getInstanceFor(propFilePath, DataProviderType.Property);
-            DataProvider animalsDataProvider = DataProviderFactory
-                    .getInstanceFor(animalsFilePath, DataProviderType.Animal);
+            propDataProvider = DataProviderFactory
+                    .getInstanceFor(Options.getPropertiesSource(), DataProviderType.Property);
+            animalsDataProvider = DataProviderFactory
+                    .getInstanceFor(Options.getAnimalsSource(), DataProviderType.Animal);
+        }
+        catch (DataProviderNotFoundException e) {
+            print("No handler for such data source");
+            exit(-2);
+        }
+
+        try {
             (new MainEngine(animalsDataProvider, propDataProvider)).run();
         }
-        catch (Exception e) {
-            System.out.println(e.getMessage());
-            System.exit(2);
+        catch (ClassNotFoundException e) { //Just suppress this
+
+        }
+        catch (DataProviderException e) {
+            print("Source reading error: " + e.getMessage());
         }
     }
 
---
+++ src/main/java/com/kmoxvilleEntertainmentGroup/Utils/HelperUtils.java
@@ -0,0 +1,11 @@
+package com.kmoxvilleEntertainmentGroup.Utils;
+
+public class HelperUtils {
+    public static void print(String str) {
+        System.out.println(str);
+    }
+
+    public static void exit(int code) {
+        System.exit(code);
+    }
+}
